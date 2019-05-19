f59593a3a8b26c9dc7b810e3936c88b35477af7d
xibo <1213425853@qq.com>
2019-05-19 22:29:04 +0800

    draw的用法练习答案

--- app/src/main/java/com/xibo/customerview/draw/Practice6DrawLineView.java
+++ app/src/main/java/com/xibo/customerview/draw/Practice6DrawLineView.java
@@ -2,22 +2,37 @@ package com.xibo.customerview.draw;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice6DrawLineView extends View {
+    private int width;//view宽
+
+    private int height;//view高
+
+
+    private Paint paint;
 
     public Practice6DrawLineView(Context context) {
         super(context);
+        init();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
     }
 
     @Override
@@ -25,5 +40,13 @@ public class Practice6DrawLineView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawLine() 方法画直线
+        width = getWidth();
+        height = getHeight();
+        paint.setStrokeWidth(10);
+
+        canvas.save();
+        canvas.translate(width / 2, height / 2);
+        canvas.drawLine(-200, -200, 200, 200, paint);
+        canvas.restore();
     }
 }
